0a9697d0467d90f7a015637f70605679c9721bd8
Tushar-1-2-3 <tushar@example.com>
2026-04-19 17:47:14 +0530

    Remove react-parallax-tilt dependency from Home2 component

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import myImg from "../../Assets/avatar.svg";
-import Tilt from "react-parallax-tilt";
 
 function Home2() {
   return (
@@ -41,9 +40,7 @@ function Home2() {
             </p>
           </Col>
           <Col md={4} className="myAvtar">
-            <Tilt>
-              <img src={myImg} className="img-fluid" alt="avatar" />
-            </Tilt>
+            <img src={myImg} className="img-fluid" alt="avatar" style={{ maxWidth: "100%" }} />
           </Col>
         </Row>
       </Container>
